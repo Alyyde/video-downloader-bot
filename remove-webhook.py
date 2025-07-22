0fdb87f52c86f6ffda2b8b57a27056dbfd21b104
Alyyde <abduganievamanzura3@gmail.com>
2025-07-22 19:17:56 +0500

    Update remove-webhook.py

--- remove-webhook.py
+++ remove-webhook.py
@@ -1,5 +1,5 @@
 import telebot
 
-bot = telebot.TeleBot("YOUR_BOT_TOKEN")  # bu yerga bot tokeningni yoz
+bot = telebot.TeleBot("7525317952:AAFZqCsmhcWpF9qX4iWX2XVmTY7gmq6M8a8")  # bu yerga bot tokeningni yoz
 bot.remove_webhook()
 print("✅ Webhook removed.")
